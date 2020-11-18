3b3b4a65bf8286b16b72b2e2ad69d00d5a9218ed
Tejas-Valame <54436185+Tejas-Valame@users.noreply.github.com>
2020-11-18 11:39:05 +0530

    Update GogoDownloader.py

--- GogoDownloader.py
+++ GogoDownloader.py
@@ -2,6 +2,8 @@
 @author Tejas Valame
 Date: 16Nov2020
 Lives in: Pune,India
+Get the exe here: https://mega.nz/file/ZDxkFIrS#DKX04SdhZDzG4h2xDcFdkqoPuK0SP_LZ4O65ZrxCgrg
+*Only works on win 64 bit*
 '''
 import string, re, os, sys
 from bs4 import BeautifulSoup as bs
@@ -9,15 +11,16 @@ import requests as rq
 
 
 def end(i):
-    input("Exception: "+ str(i)+"\nHit Enter to EXIT")
+    input("Exception: " +str(i) +"\nHit Enter to EXIT")
     exit()
+
 
 def cd():	#creates a directory to store our downloaded files
     try:
         os.mkdir(name+"-"+dict1[qty])
-        print("Created New Directory: ", name+"-"+dict1[qty])
+        print("Created New Directory:", name +"-" +dict1[qty])
     except FileExistsError:
-        print("Continuing to download in Existing Folder: ", name+"-"+dict1[qty])
+        print("Continuing to download in Existing Folder:", name+"-"+dict1[qty])
     except:
         print("Cant create directory")
         end("dir creation error")
@@ -26,7 +29,7 @@ def parser(url1, txt):	#parses the hml and reurns url that contains txt
     pg=rq.get(url1)
     
     if(pg.status_code!=200):
-        print("HTTP Error, Code", str(pg.status_code))
+        print("HTTP Error, Code:", str(pg.status_code))
         return None
     else:
         soup=bs(pg.text, 'html.parser')
@@ -45,7 +48,7 @@ def correct(str):	#formats the input from line 77 to correct URL [Valid inputs '
 
 def store(url): #saves the files in folder
     print("Download Link:",url)
-    response = rq.get(url, stream=True)
+    response = rq.get(url, stream=True, timeout=10)
     total_length = response.headers.get('content-length')
 
     if total_length is None: # no content length header
@@ -55,22 +58,18 @@ def store(url): #saves the files in folder
         with open(file_name, "wb") as f:
             dl = 0
             total_length = int(total_length)
+            print("Download Size:",int(total_length/1048576)+1, "MB")
             for data in response.iter_content(chunk_size=5120):
                 dl += len(data)
                 f.write(data)
                 done = int(50 * dl / total_length)
-                sys.stdout.write("\r["+'=' * done+' ' * (50-done)+"]  "+str(dl/total_length*100)[:5]+" %" )    #displaysdownload percentage and progress bar
+                #The following line displays download percentage and progress bar
+                sys.stdout.write("\r["+'=' * done+' ' * (50-done)+"] "+str(dl/1048576)[:6]+" MB/"+str(total_length/1048576)[:6]+ " MB "+str(dl/total_length*100)[:5]+"%" )   
                 sys.stdout.flush()
-
-
-
-
-
-
 
 dict1={ 1:"360p", 2:"480p", 3:"720p", 4:"1080p", }
 epf=0
-baseURL=correct(input("Enter the Base URL from gogo anime: "))
+baseURL=correct(input("Enter the URL from gogo anime: "))
 
 try:
     base_ep=int(input("Enter episode number to begin with: "))
@@ -78,11 +77,12 @@ try:
     if base_ep>end_ep:
         end("Wrong Numbers")
     print("Menu:\n 1: 360p\t 2: 480p\t 3: 720p\t 4: 1080p")
-    qty=int(input("Enter Episode Download Quality:"))
+    qty=int(input("Enter Episode Download Quality: "))
     if(qty not in range(1,5)):
         end("Undefined Quality")
 except:
     end("Baka")
+
 lst=baseURL.split('/')
 name=lst[4]
 
@@ -94,22 +94,26 @@ try:
         print("Episode URL:", ep_URL)
         dpg=parser(ep_URL, "https://gogo-stream.com/download")
         if(dpg==None):
-            print("Could not access dgp: ", ep_URL, "Moving Next")
+            print("Could not access download page:", ep_URL, "Try it manually. Moving Next")
             epf+=1
             continue
         print(name,"episode", str(ep_num),":",dpg)
-        dl=parser(dpg, dict1[qty])
+        try:
+            dl=parser(dpg, dict1[qty])
+        except Exception:
+            pass
+        
         if(dl==None):
-            print("Could not access dl: ", "Moving Next")
+            print(name+"-episode-"+str(ep_num), "is not available in", dict1[qty]+". Try some other resolution. Moving Next")
             epf+=1
             continue
         else:
             try:
                 store(dl.replace("amp;", ""))
-            except:
+            except Exception:
                 epf+=1
     print("\n"+str(end_ep-base_ep-epf+1), "Succeded", epf, "failed")
     input("\nDownload Completed.\n Happy Watching\nHit Enter to Exit")
-except Exception:
-    print("\n"+str(end_ep-base_ep-epf), "Succeded\t", epf, "Failed")
-    input("\nSome Fatal Error has occured\nHit Enter to Exit")
+except Exception as ex:
+    print(ex)
+    input("\nSome Fatal Error has occured. Try downloading in other quality or try manually\nHit Enter to Exit")
